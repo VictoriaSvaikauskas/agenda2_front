28ea24e7644e9964f48e1e700197f67d1e3a23a2
Juani Queral <queraljuani@gmail.com>
2022-10-18 19:19:18 -0300

    18/10

---
+++ src/app/core/services/user.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { ContactJsonPlaceholder } from '../interfaces/contacts';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  constructor() {}
+
+  async getUserDetails(id: number): Promise<ContactJsonPlaceholder> {
+    const jsonData = await this.getUsers();
+    const user = jsonData.filter((user) => user.id == id);
+    return user.length > 0 ? user[0] : {};
+  }
+
+  async getUsers(): Promise<ContactJsonPlaceholder[]> {
+    const data = await fetch('https://jsonplaceholder.typicode.com/users');
+    return await data.json();
+  }
+
+  async editContact(contact: ContactJsonPlaceholder) {
+    console.log('Enviando edit de usuario a la api');
+    const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(contact),
+    });
+    return await res.json();
+  }
+
+  async addContact(contact: ContactJsonPlaceholder){
+    console.log('Enviando edit de usuario a la api');
+    const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(contact),
+    });
+    return await res.json();
+  }
+  
+  async DeleteContact(contact: ContactJsonPlaceholder) {
+    console.log('Enviando edit de usuario a la api');
+    const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'Del',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(contact),
+    });
+    return await res.json();
+  }
+  
+}
